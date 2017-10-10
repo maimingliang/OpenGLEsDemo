1b693ad494b1bc7e1ed706e7208dc52c431167d3
maimingliang <maimingliang8@163.com>
2017-10-10 23:42:16 +0800

    修改纹理贴图

--- app/src/main/assets/fshader/filter_fragment.sh
+++ app/src/main/assets/fshader/filter_fragment.sh
@@ -5,6 +5,7 @@ uniform sampler2D sTexture;
 
 varying vec4 aPos;
 uniform vec3 vChangeColor;
+uniform int vChangeType;
 
 void modifyColor(vec4 color){
     color.r=max(min(color.r,1.0),0.0);
@@ -18,23 +19,30 @@ void modifyColor(vec4 color){
 void main() {
 
 
-  vec4 nColor = texture2D(sTexture,vTexCoord);
+
+    vec4 nColor = texture2D(sTexture,vTexCoord);
+
+    if(aPos.x > 0.0){
+
+        if(vChangeType == 1){
+            float c=nColor.r*vChangeColor.r+nColor.g*vChangeColor.g+nColor.b*vChangeColor.b;
+            gl_FragColor=vec4(c,c,c,nColor.a);
+        }else if(vChangeType == 2){
+
+                    vec4 deltaColor=nColor+vec4(vChangeColor,0.0);
+                    modifyColor(deltaColor);
+                    gl_FragColor=deltaColor;
+
+        }else{
+             gl_FragColor = nColor;
+        }
 
 
-   if(aPos.x > 0.0){
-
-        vec4 deltaColor=nColor+vec4(vChangeColor,0.0);
-         modifyColor(deltaColor);
-        gl_FragColor=deltaColor;
-
-
-   }else{
-
-    gl_FragColor =nColor；
-
-   }
 
 
 
+    }else{
+         gl_FragColor = nColor;
+    }
 
 }
--- app/src/main/java/com/maiml/openglesdemo/renderer/FilterRenderer.java
+++ app/src/main/java/com/maiml/openglesdemo/renderer/FilterRenderer.java
@@ -11,6 +11,7 @@ import com.maiml.openglesdemo.R;
 import com.maiml.openglesdemo.filter.Filter;
 import com.maiml.openglesdemo.utils.ShaderUtils;
 import com.maiml.openglesdemo.utils.TextureHelper;
+import com.maiml.openglesdemo.utils.VaryTools;
 
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
@@ -61,6 +62,8 @@ public class FilterRenderer implements GLSurfaceView.Renderer {
     private int aTextureCoordHandle;
     private int textureId;
     private int vChangeColorHandle;
+    private int vChangeTypeHandle;
+    private final VaryTools varyTools;
 
     public void setImageBuffer(int[] buffer,int width,int height){
         mBitmap= Bitmap.createBitmap(buffer,width,height, Bitmap.Config.RGB_565);
@@ -92,7 +95,7 @@ public class FilterRenderer implements GLSurfaceView.Renderer {
     private  int mFragmentShader;
      public FilterRenderer(Context context) {
         mContext = context;
-
+         varyTools = new VaryTools();
         ByteBuffer bb = ByteBuffer.allocateDirect(
                 vertexData.length * 4);
         bb.order(ByteOrder.nativeOrder());
@@ -131,8 +134,10 @@ public class FilterRenderer implements GLSurfaceView.Renderer {
         mPositionHandle = GLES20.glGetAttribLocation(mProgram, "aPosition");
          uMatrixHandle=GLES20.glGetUniformLocation(mProgram,"uMatrix");
         aTextureCoordHandle=GLES20.glGetAttribLocation(mProgram,"aTexCoord");
-        vChangeColorHandle=GLES20.glGetAttribLocation(mProgram,"vChangeColor");
+        vChangeColorHandle=GLES20.glGetUniformLocation(mProgram,"vChangeColor");
+        vChangeTypeHandle=GLES20.glGetUniformLocation(mProgram,"vChangeType");
         uTextureSamplerHandle=GLES20.glGetUniformLocation(mProgram,"sTexture");
+
 
 //        textureId = TextureHelper.loadTexture(mContext, R.mipmap.image01);
         BitmapFactory.Options options=new BitmapFactory.Options();
@@ -165,25 +170,37 @@ public class FilterRenderer implements GLSurfaceView.Renderer {
         if (width > height) {
             //横屏
                 if(sRatio > bRatio){
-                    Matrix.orthoM(mProjectMatrix, 0, -sRatio/bRatio,sRatio/bRatio, -1,1, 3, 7);
+//                    Matrix.orthoM(mProjectMatrix, 0, -sRatio/bRatio,sRatio/bRatio, -1,1, 3, 7);
+                    varyTools.ortho(-sRatio/bRatio,sRatio/bRatio, -1,1, 3, 7);
 
                 }else{
-                    Matrix.orthoM(mProjectMatrix, 0, -sRatio*bRatio,sRatio*bRatio, -1,1, 3, 7);
+//                    Matrix.orthoM(mProjectMatrix, 0, -sRatio*bRatio,sRatio*bRatio, -1,1, 3, 7);
+                    varyTools.ortho(-sRatio*bRatio,sRatio*bRatio, -1,1, 3, 7);
                 }
         }else{
             //竖屏
             if(sRatio > bRatio){
-                Matrix.orthoM(mProjectMatrix, 0, -1,1,-bRatio/sRatio,bRatio/sRatio, 3, 7);
+//                Matrix.orthoM(mProjectMatrix, 0, -1,1,-bRatio/sRatio,bRatio/sRatio, 3, 7);
+                varyTools.ortho(-1,1,-bRatio/sRatio,bRatio/sRatio, 3, 7);
             }else{
-                Matrix.orthoM(mProjectMatrix, 0, -1,1, -1/sRatio*bRatio,1/sRatio*bRatio, 3, 7);
+//                Matrix.orthoM(mProjectMatrix, 0, -1,1, -1/sRatio*bRatio,1/sRatio*bRatio, 3, 7);
+                varyTools.ortho(-1,1, -1/sRatio*bRatio,1/sRatio*bRatio, 3, 7);
             }
         }
 
-        //设置相机位置
-        Matrix.setLookAtM(mViewMatrix, 0, 0, 0, 7.0f, 0f, 0f, 0f, 0f, 1.0f, 0.0f);
-        //计算变换矩阵
-        Matrix.multiplyMM(mMVPMatrix,0,mProjectMatrix,0,mViewMatrix,0);
+         varyTools.setCamera(0, 0, 7.0f, 0f, 0f, 0f, 0f, 1.0f, 0.0f);
+//        //设置相机位置
+//        Matrix.setLookAtM(mViewMatrix, 0, 0, 0, 7.0f, 0f, 0f, 0f, 0f, 1.0f, 0.0f);
+//        //计算变换矩阵
+//        Matrix.multiplyMM(mMVPMatrix,0,mProjectMatrix,0,mViewMatrix,0);
 
+
+         //y轴正方形平移
+        varyTools.pushMatrix();
+//        varyTools.translate(0,3,0);
+//         varyTools.rotate(30f,-1,-1,1);
+//         varyTools.popMatrix();
+         varyTools.scale(2.0f,2.0f,2.0f);
     }
 
     @Override
@@ -194,7 +211,9 @@ public class FilterRenderer implements GLSurfaceView.Renderer {
         //将程序加入到OpenGLES2.0环境
         GLES20.glUseProgram(mProgram);
 
-        GLES20.glUniformMatrix4fv(uMatrixHandle,1,false,mMVPMatrix,0);
+
+        GLES20.glUniformMatrix4fv(uMatrixHandle,1,false,varyTools.getFinalMatrix(),0);
+
 
         //启用三角形顶点的句柄
         GLES20.glEnableVertexAttribArray(mPositionHandle);
@@ -204,6 +223,8 @@ public class FilterRenderer implements GLSurfaceView.Renderer {
                 12, vertexBuffer);
 
         GLES20.glUniform3fv(vChangeColorHandle,1, Filter.GRAY.data(),0);
+        GLES20.glUniform1i(vChangeTypeHandle,Filter.GRAY.getType());
+
         //获取片元着色器的vColor成员的句柄
         GLES20.glEnableVertexAttribArray(aTextureCoordHandle);
         GLES20.glVertexAttribPointer(aTextureCoordHandle,2,GLES20.GL_FLOAT,false,8,coordBuffer);
